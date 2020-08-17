39a2a8401d6659f59b633ae06a1560a807669c7e
c4324r <baconlord123456@gmail.com>
2020-08-16 17:22:58 -0700

    Movement Prototype
    This is the prototype of the movement in Atrioc Racing, a moba like movement rather than rts since I can't find out pathfinding.

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 target;
+    private Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = new Vector2(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+}
